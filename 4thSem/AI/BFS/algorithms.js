04a9ca63e7f007c3a2eb8a017446bf804183b800
leyuskckiran1510 <kdhakal1510@gmail.com>
2023-12-08 10:51:59 +0545

    minor fixes

--- 4thSem/AI/BFS/algorithms.js
+++ 4thSem/AI/BFS/algorithms.js
@@ -9,10 +9,12 @@ var nodeCollection = new Array()
 var LAST_USED = false
 var old_nodeCollection = new Array()
 var rootNode = null
+var destination =null
 var COLORS = ["red", "green", "yellow", "blue"]
-var  knockBack = "#4ae54a"
+var  knockBack = "#003d00"
 var timeOuts = []
 var BFS = 1
+var INFORMED = 0
 
 class Node {
     constructor(value, type, ix, iy) {
@@ -28,7 +30,9 @@ class Node {
 
 let sleep = (x) => {
     var now = new Date().getTime();
-    while (new Date().getTime() < now + x) { /* Do nothing */ }
+    while (new Date().getTime() < now + x) {
+     /* Do nothing */
+      }
 }
 
 
@@ -45,47 +49,56 @@ let direcs = [
     // [-1, 1],
 ]
 
+let paint_me_please = (rows,cols)=>{
+    for (let row = 0; row < rows; row++) {
+            for (let col = 0; col < cols; col++) {
+                if(nodeCollection[row][col].type!=PATH_NODE){
+                    colorCellByIndex(canvas, context,
+                     nodeCollection[row][col].ix, nodeCollection[row][col].iy,
+                      COLORS[nodeCollection[row][col].type])
+
+                }
+        }
+    }
+}
+
 let createNodes = (rows, cols) => {
     nodeCollection = new Array()
     //create Nodes
     print("Making Nodes")
-    if (LAST_USED) {
-        LAST_USED = false
-        nodeCollection = JSON.parse(JSON.stringify(old_nodeCollection))
-    } else {
-        rootNode = null
-        let destination = null
-        for (let row = 0; row < rows; row++) {
-            let _temp = []
-            for (let col = 0; col < cols; col++) {
-                let [px, py] = GRID.pos_from_index(col, row)
-                let _colValue = context.getImageData(px + GRID.width / 2, py + GRID.height / 2, 1, 1).data
-                if (_colValue[0] >= 200 && _colValue[1] >= 200 && _colValue[2] <= 100) {
-                    _temp.push(new Node(col * row, WALL_NODE, col, row))
-                } else if (_colValue[0] >= 200 &&
-                    _colValue[1] <= 100 &&
-                    _colValue[2] <= 100 &&
-                    rootNode == null) {
-                    let _rt = new Node(col * row, ROOT_NODE, col, row)
-                    rootNode = _rt;
-                    _temp.push(rootNode)
-                } else if (_colValue[0] <= 100 &&
-                    _colValue[1] <= 100 &&
-                    _colValue[2] >= 200 &&
-                    destination == null) {
-                    destination = [col, row];
-                    _temp.push(new Node(col * row, FINAL_NODE, col, row))
+    rootNode = null
+    destination = null
+    for (let row = 0; row < rows; row++) {
+        let _temp = []
+        for (let col = 0; col < cols; col++) {
+            let [px, py] = GRID.pos_from_index(col, row)
+            let _colValue = context.getImageData(px + GRID.width / 2, py + GRID.height / 2, 1, 1).data
+            if (_colValue[0] >= 200 && _colValue[1] >= 200 && _colValue[2] <= 100) {
+                _temp.push(new Node(col * row, WALL_NODE, col, row))
+            } else if (_colValue[0] >= 200 &&
+                _colValue[1] <= 100 &&
+                _colValue[2] <= 100 &&
+                rootNode == null) {
+                let _rt = new Node(col * row, ROOT_NODE, col, row)
+                rootNode = _rt;
+                _temp.push(rootNode)
+            } else if (_colValue[0] <= 100 &&
+                _colValue[1] <= 100 &&
+                _colValue[2] >= 200 &&
+                destination == null) {
+                destination = new Node(col * row, FINAL_NODE, col, row);
+                _temp.push(destination)
 
-                }
-                else {
-                    _temp.push(new Node(col * row, PATH_NODE, col, row))
-
-                }
             }
-            nodeCollection.push(_temp)
+            else {
+                _temp.push(new Node(col * row, PATH_NODE, col, row))
+
+            }
         }
-        old_nodeCollection = JSON.parse(JSON.stringify(nodeCollection))
+        nodeCollection.push(_temp)
     }
+    old_nodeCollection = JSON.parse(JSON.stringify(nodeCollection))
+    depth =0
     make_child(rootNode)
 
     graphDim = [cols, rows]
@@ -100,14 +113,64 @@ let shuffel = (list)=>{
     }
     return _temp
 }
+[
+    [1, 0],
+    [0, 1],
+    [0, -1],
+    [-1, 0],
+]
+
+let find=(lis,item)=>{
+    for(let x=0;x<lis.length;x++){
+        if(lis[x]==item){
+            return x
+        }
+    }
+    return -1
+}
+
+let  adjustDirection = (curent,direcs)=>{
+    let _temp = []
+    if(destination.ix>=curent.ix){
+        let index = find(direcs,[1,0])
+        direcs.splice(index,1)
+        _temp.push([1,0])
+    }
+     if(destination.iy>=curent.iy){
+        let index = find(direcs,[0,1])
+        direcs.splice(index,1)
+        _temp.push([0,1])
+    }
+     if(destination.ix<curent.ix){
+        let index = find(direcs,[-1,0])
+        direcs.splice(index,1)
+        _temp.push([-1,0])
+    }
+     if(destination.iy<curent.iy){
+        let index = find(direcs,[0,-1])
+        direcs.splice(index,1)
+        _temp.push([0,-1])
+    }
+    while(direcs.length>0){
+        _temp.push(direcs.pop())
+    }
+    return _temp
+}
+var depth =0
 let make_child = (root) => {
+    depth++
     let childs = [root]
     let dfsChild = []
     for (let child of childs) {    
         if(child==null){
             continue
         }
-        for (dir of direcs) {
+        if(INFORMED){
+            direcs  = adjustDirection(child,direcs)
+        }else{
+            direcs = [[1, 0],[0, 1],[0, -1],[-1, 0]]
+        }
+        for (let dir of direcs) {
             let [x, y] = [child.ix + dir[0], child.iy + dir[1]]
             if (
                 x >= 0 && 
@@ -128,7 +191,8 @@ let make_child = (root) => {
                     if (BFS) {
                         childs.push(nodeCollection[y][x])
                     } else {
-                        dfsChild.push(nodeCollection[y][x])
+                        dfsChild.unshift(nodeCollection[y][x])
+                        // make_child(nodeCollection[y][x])
                     }
                 }
                 
@@ -169,7 +233,7 @@ let bFs = (...root_node) => {
 
         if (_childs.length > 0) {
             colorCellByIndex(canvas, context, visted.ix, visted.iy, COLORS[visted.type])
-        } else if (visted.type==PATH_NODE){
+        } else if (visted.type!=PATH_NODE){
             colorCellByIndex(canvas, context, visted.ix, visted.iy, knockBack)
         }
         visted.traversed = true
@@ -195,7 +259,7 @@ let dFs = (root_node) => {
             x.traversed = true
             let id = setTimeout(() => {
                 let _has_child = 0
-                if (x.childs.length > 0) {
+                if (x.childs.length > 0 || x.type!=PATH_NODE) {
                     colorCellByIndex(canvas, context, x.ix, x.iy, COLORS[x.type])
                     _has_child = 1
                 } else {
@@ -205,7 +269,7 @@ let dFs = (root_node) => {
                     for (let _m of timeOuts) {
                         clearTimeout(_m)
                     }
-                    travelBack(x)
+                    travelBack(x.parent)
                     return
                 }
                 if (_has_child) {
